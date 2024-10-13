a5342a909e0a399dee8f11e60f5dd72374b7dd83
shinis(home) <actionshin@gmail.com>
2024-10-14 01:25:43 +0900

    After 어드바이스

--- src/main/java/kr/co/cofile/sbfirst/chapter03/aop/SampleAspect.java
+++ src/main/java/kr/co/cofile/sbfirst/chapter03/aop/SampleAspect.java
@@ -1,6 +1,7 @@
 package kr.co.cofile.sbfirst.chapter03.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -8,13 +9,23 @@ import org.springframework.stereotype.Component;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+// 애스펙트
 @Aspect
 @Component
 public class SampleAspect {
 
+    // 조인트포인트 - 포인트컷(반환값, 패키지.클래스.메서드(인수))
     @Before("execution(* kr.co.cofile.sbfirst.chapter03.used.*Greet.*(..))")
+    // 어드바이스
     public void beforeAdvice(JoinPoint joinPoint) {
         System.out.println("===== Before Advice =====");
+        System.out.println(new SimpleDateFormat("yyyy/MM/dd").format(new Date()));
+        System.out.printf("메서드:%s\n", joinPoint.getSignature().getName());
+    }
+
+    @After("execution(* kr.co.cofile.sbfirst.chapter03.used.*Greet.*(..))")
+    public void afterAdvice(JoinPoint joinPoint) {
+        System.out.println("===== After Advice =====");
         System.out.println(new SimpleDateFormat("yyyy/MM/dd").format(new Date()));
         System.out.printf("메서드:%s\n", joinPoint.getSignature().getName());
     }
